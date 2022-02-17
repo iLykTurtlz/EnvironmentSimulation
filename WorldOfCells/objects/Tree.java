028539b107eb4af70c70a6082e4f348a68643f1c
Chanattan <zentsugo@gmail.com>
2022-02-17 11:46:40 +0100

    Fix trees not shown

--- WorldOfCells/objects/Tree.java
+++ WorldOfCells/objects/Tree.java
@@ -35,15 +35,15 @@ public class Tree extends CommonObject {
     		
     		// maj: 2020-02-13
     		
+    		gl.glVertex3f( offset+x*stepX, offset+y*stepY, altitude );
             gl.glVertex3f( offset+x*stepX-lenY/16.f, offset+y*stepY+lenY/2.f, altitude + 4.f );
-    		gl.glVertex3f( offset+x*stepX, offset+y*stepY, altitude );
+            gl.glVertex3f( offset+x*stepX, offset+y*stepY, altitude );
             gl.glVertex3f( offset+x*stepX+lenY/16.f, offset+y*stepY-lenY/2.f, altitude + 4.f );
-            gl.glVertex3f( offset+x*stepX, offset+y*stepY, altitude );
 
-            gl.glVertex3f( offset+x*stepX-lenY/2.f, offset+y*stepY+lenY/16.f, altitude + 4.f );
     		gl.glVertex3f( offset+x*stepX, offset+y*stepY, altitude );
-            gl.glVertex3f( offset+x*stepX+lenY/2.f, offset+y*stepY-lenY/16.f, altitude + 4.f );
+            gl.glVertex3f( offset+x*stepX-lenY/2.f, offset+y*stepY+lenY/16.f, altitude + 4.f );
             gl.glVertex3f( offset+x*stepX, offset+y*stepY, altitude );
+            gl.glVertex3f( offset+x*stepX+lenY/2.f, offset+y*stepY-lenY/16.f, altitude + 4.f );
         }
     }
 

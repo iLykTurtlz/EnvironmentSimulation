e283d64113133e014dc5749e8b55db1619da0481
Chanattan <zentsugo@gmail.com>
2022-04-08 23:56:54 +0200

    second version of volcano the lava spreads wider

--- WorldOfCells/applications/simpleworld/LavaCA.java
+++ WorldOfCells/applications/simpleworld/LavaCA.java
@@ -2,40 +2,32 @@ package applications.simpleworld;
 
 import cellularautomata.CellularAutomataDouble;
 import cellularautomata.CellularAutomataInteger;
-import worlds.World;
+import applications.simpleworld.WorldOfTrees;
 
 public class LavaCA extends CellularAutomataInteger {
 
-	private World world;
+	private WorldOfTrees world;
 	private static final int ROCK_TIME = 30;
+	private static final double SPREAD_CHANCE = 0.3d;
 
-	public LavaCA ( World __world, int __dx , int __dy )
+	public LavaCA ( WorldOfTrees __world, int __dx , int __dy )
 	{
 		super(__dx,__dy,true );
 		this.world = __world;
 	}
 
+    //lava is set manually by the volcano
 	public void init()
 	{
-		/*for ( int x = 0 ; x != _dx ; x++ )
-    		for ( int y = 0 ; y != _dy ; y++ )
-    		{
-    			if ( _cellsHeightValuesCA.getCellState(x,y) >= WorldOfTrees.SNOW_LINE )
-    			{
-    				if ( Math.random() < 1 )
-    					this.setCellState(x, y, 1); // lava
-    				else
-    					this.setCellState(x, y, 0); // empty
-    			}
-    			else
-    			{
-    				this.setCellState(x, y, -1); // water (ignore)
-    			}
-    		}*/
     	this.swapBuffer();
-
 	}
 
+    /**
+      * The Lava Cellular Automata is basically a cell that is affected by gravity and more precisely by height difference,
+      * the lava will flow until reaching a stable height (neighborrs at same level). Up to a certain point the
+      * lava itself will turn into stone (lava lifespan). The lava will directly turn into stone if it reaches water.
+      * The lava lifespan is ROCK_TIME, it decreases with time and with world iterations % 10.
+      */
 	public void step()
 	{
     	for ( int i = 0 ; i != _dx ; i++ )
@@ -46,66 +38,115 @@ public class LavaCA extends CellularAutomataInteger {
     			{
 	    			if ( this.getCellState(i,j) == 1 ) // lava
 	    			{
-	    				// check if neighbors are at a lower height
+	    				// get neighbors height
 	    				float left = (float) world.getCellHeight((i+_dx-1)%(_dx), j);
 	    				float right = (float) world.getCellHeight((i+_dx+1)%(_dx), j);
 	    				float up = (float) world.getCellHeight(i , (j+_dy+1)%(_dy));
 	    				float down = (float) world.getCellHeight(i , (j+_dy-1)%(_dy));
 
-	    						/*left < height ||
-	    						right < height ||
-	    						up < height ||
-	    						down < height*/
-                        // check if neighbors are lava and the lava is falling
+                        // get the state of the neighbors
                         int lstate = this.getCellState( (i+_dx-1)%(_dx) , j );
                         int rstate = this.getCellState( (i+_dx+1)%(_dx) , j );
                         int ustate = this.getCellState( i , (j+_dy+1)%(_dy) );
                         int dstate = this.getCellState( i , (j+_dy-1)%(_dy) );
-                            //look for the minimum height
-                            int x, y;
-                            float min = left;
-                            int state = lstate;
-                            if (min > right) {
+
+                        //look for the minimum height among neighbors
+                        int x, y;
+                        float min = left;
+                        int mini = 0;
+                        int state = lstate;
+                        int count = 0;
+                        float[][] dir = new float[4][3];
+
+                        if (left < height) {
+                            count++;
+                            dir[0][0] = (i+_dx-1)%(_dx);
+                            dir[0][1] = j;
+                            dir[0][2] = left;
+                        }
+
+                        if (right < height) {
+                            count++;
+                            dir[1][0] = (i+_dx+1)%(_dx);
+                            dir[1][1] = j;
+                            dir[1][2] = right;
+                            if (right < min) {
                                 min = right;
-                                state = rstate;
+                                mini = 1;
                             }
-                            if (min > up) {
+                        }
+
+                        if (up < height) {
+                            count++;
+                            dir[2][0] = i;
+                            dir[2][1] = (j+_dy+1)%(_dy);
+                            dir[2][2] = up;
+                            if (up < min) {
                                 min = up;
-                                state = ustate;
+                                mini = 2;
                             }
-                            if (min > down) {
+                        }
+
+                        if (down < height) {
+                            count++;
+                            dir[3][0] = i;
+                            dir[3][1] = (j+_dy-1)%(_dy);
+                            dir[3][2] = down;
+                            if (down < min) {
                                 min = down;
-                                state = dstate;
+                                mini = 3;
                             }
+                        }
 
-                            if (min == left) {
-                                x = (i+_dx-1)%(_dx);
-                                y = j;
-                            } else if (min == right) {
-                                x = (i+_dx+1)%(_dx);
-                                y = j;
-                            } else if (min == up) {
-                                x = i;
-                                y = (j+_dy+1)%(_dy);
-                            } else {
-                                x = i;
-                                y = (j+_dy-1)%(_dy);
-                            }
+                        /*if (min > right) {
+                            min = right;
+                            state = rstate;
+                        }
+                        if (min > up) {
+                            min = up;
+                            state = ustate;
+                        }
+                        if (min > down) {
+                            min = down;
+                            state = dstate;
+                        }
 
-                            if ((state == 0 || state == -1 || state == 2) && min < height) {
-                                if (min >= WorldOfTrees.WATER_LEVEL)
-                                    this.setCellState(x, y, 1); //set lava
-                                else
-                                    this.setCellState(x, y, 2); //set rock
+                        if (min == left) {
+                            x = (i+_dx-1)%(_dx);
+                            y = j;
+                        } else if (min == right) {
+                            x = (i+_dx+1)%(_dx);
+                            y = j;
+                        } else if (min == up) {
+                            x = i;
+                            y = (j+_dy+1)%(_dy);
+                        } else {
+                            x = i;
+                            y = (j+_dy-1)%(_dy);
+                        }*/
+                        state = this.getCellState((int) dir[mini][0], (int) dir[mini][1]);
+                        if ((state == 0 || state == 2) && count > 0) {
+                            world.getForest().swapBuffer(); // buffer has been changed by the previous call we need to come back to it
+                            flowLava((int) dir[mini][0], (int) dir[mini][1], dir[mini][2]);
+                            double r = Math.random();
+                            while (r < SPREAD_CHANCE) {
+                                int ind = (int)(Math.random()*count);
+                                flowLava((int) dir[ind][0], (int) dir[ind][1], dir[ind][2]);
+                                r = Math.random();
                             }
-                            this.setCellState(i, j, 3); //set current cell to 2 nd lava state
-                            /*if (this.getCellState( (i+_dx+1)%(_dx) , j ) == -1 && right < height)
-                                this.setCellState( (i+_dx+1)%(_dx), j , 1);
-                            if (this.getCellState( i , (j+_dy+1)%(_dy) ) == -1 && up < height)
-                                this.setCellState( i , (j+_dy+1)%(_dy), 1);
-                            if (this.getCellState( i , (j+_dy-1)%(_dy) ) == -1 && down < height)
-                                this.setCellState( i , (j+_dy-1)%(_dy), 1);*/
-                            //this.setCellState(i, j, this.getCellState(i, j)+1); // lava 2 nd state
+                        }
+
+                        /*if ((state == 0 || state == -1 || state == 2) && min < height) {
+                            if (min >= WorldOfTrees.WATER_LEVEL) {
+                                this.setCellState(x, y, 1); //set lava
+                                if (world.getCellValue(x, y) == 1) { // if lava is flowing on tree then it burns
+                                    world.getForest().swapBuffer(); // buffer has been changed by the previous call we need to come back to it
+                                    world.setCellValue(x, y, 2);
+                                }
+                            } else
+                                this.setCellState(x, y, 2); //set rock
+                        }*/
+                        this.setCellState(i, j, 3); //set current cell to 2 nd lava state
 	    			}
 	    			else
 	    			{
@@ -126,9 +167,9 @@ public class LavaCA extends CellularAutomataInteger {
                         color[1] = (float) (0.3d * r) + 0.3f;
                         color[2] = 0.f;
                     } else if (this.getCellState(i, j) == 2) {
-                        color[0] = 0.3f * height + 0.2f;
-                        color[1] = 0.3f * height + 0.2f;
-                        color[2] = 0.3f * height + 0.2f;
+                        color[0] = 0.3f * (height+.1f) + 0.2f;
+                        color[1] = 0.3f * (height+.1f) + 0.2f;
+                        color[2] = 0.3f * (height+.1f) + 0.2f;
                     } else {
                         color[0] = 0.5f;
                         color[1] = 0.5f;
@@ -142,5 +183,14 @@ public class LavaCA extends CellularAutomataInteger {
     	this.swapBuffer();
 	}
 
-
+    private void flowLava(int x, int y, float height) {
+        if (height >= WorldOfTrees.WATER_LEVEL) {
+            this.setCellState(x, y, 1); //set lava
+            if (world.getCellValue(x, y) == 1) { // if lava is flowing on tree then it burns
+                world.setCellValue(x, y, 2);
+            }
+        } else {
+            this.setCellState(x, y, 2); //set rock
+        }
+    }
 }
